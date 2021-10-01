da461996b1d3e48d0ccfe8101912c96b7a11c830
leenawon <nawon5154@gmail.com>
2021-10-02 03:11:40 +0900

    Header component 생성 후 App.js에 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div>
-      happy
+    <div className="ui container">
+      {/* Header Component */}
+      <Header />
     </div>
   );
 }
