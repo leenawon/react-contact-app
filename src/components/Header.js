7d7241b619946454e25ac98da9430e93e25354e8
leenawon <nawon5154@gmail.com>
2021-10-03 00:57:23 +0900

    Header Component 작성 (Header.js)

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="ui container">
+        <h2 className="ui center aligned grey icon header">
+          <i className="circular users icon"></i>
+          나만의 연락처 리스트
+        </h2>
+    </div>
+  )
+}
+
+export default Header;
