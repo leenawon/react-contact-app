7361b2c40e53eb37c8279565bb38072d251b9097
leenawon <nawon5154@gmail.com>
2021-10-06 17:18:14 +0900

    ContactList component App.js에 추가 및 contacts array 작성

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import './App.css';
 import AddContact from './components/AddContact';
+import ContactList from './components/ContactList';
 import Header from './components/Header';
 
 function App() {
+  const contacts = [
+    {
+      id: "1",
+      "name": "naoni",
+      "email": "naoni@gmail.com"
+    },
+    {
+      id: "2",
+      "name": "daoni",
+      "email": "daoni@gmail.com"
+    },
+    {
+      id: "3",
+      "name": "kyoungho",
+      "email": "kyoungho@gmail.com"
+    }
+  ];
+
   return (
     <div className="ui container">
       {/* Header Component */}
       <Header />
       {/* Add Contact Component */}
       <AddContact />
+      {/* Contact List Component */}
+      <ContactList contacts={contacts} />
     </div>
   );
 }
