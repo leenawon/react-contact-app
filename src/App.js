551c9ab052b14c53a7705f012f4f327b7523a5d8
leenawon <nawon5154@gmail.com>
2021-10-15 01:47:03 +0900

    addContact 함수 만들어서 AddContact component props로 전달

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import AddContact from './components/AddContact';
 import ContactList from './components/ContactList';
 import Header from './components/Header';
 
 function App() {
-  const contacts = [
-    {
-      id: "1",
-      "name": "naoni",
-      "email": "naoni@gmail.com"
-    },
-    {
-      id: "2",
-      "name": "daoni",
-      "email": "daoni@gmail.com"
-    },
-    {
-      id: "3",
-      "name": "kyoungho",
-      "email": "kyoungho@gmail.com"
-    }
-  ];
+  const [contacts, setContacts] = useState([]);
+
+  function addContact(contact) {
+    setContacts([...contacts, contact]);
+  }
 
   return (
     <div className="ui container">
       {/* Header Component */}
       <Header />
       {/* Add Contact Component */}
-      <AddContact />
+      <AddContact addContact={addContact} />
       {/* Contact List Component */}
       <ContactList contacts={contacts} />
     </div>
