a30dfc11d884d1b73e3af0aba689c42547486d08
leenawon <nawon5154@gmail.com>
2021-10-16 00:31:33 +0900

    useState 이용 form에 입력한 값 addContact로 전달

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function AddContact() {
+function AddContact(props) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const contact = {name, email};
+
+  function nameOnChange(e) {
+    setName(e.target.value);
+  }
+
+  function emailOnChange(e) {
+    setEmail(e.target.value);
+  }
+
+  function formSubmitHandler(e) {
+    e.preventDefault();
+    if(name === "" || email === "") {
+      alert('모든 항목을 입력해주세요!');
+    }
+
+    setName("");
+    setEmail("");
+    props.addContact(contact);
+  }
+
   return (
     <div className="ui container">
-      <form className="ui form">
+      <form className="ui form" onSubmit={formSubmitHandler}>
         <div className="required field">
           <label>이름</label>
-          <input type="text" name="name" placeholder="이름을 입력하세요"></input>
+          <input type="text" name="name" placeholder="이름을 입력하세요" value={name} onChange={nameOnChange}></input>
         </div>
         <div className="required field">
           <label>이메일</label>
-          <input type="text" name="email" placeholder="이메일 주소를 입력하세요"></input>
+          <input type="text" name="email" placeholder="이메일 주소를 입력하세요" value={email} onChange={emailOnChange}></input>
         </div>
         <button className="ui inverted grey button">추가</button>
       </form>
