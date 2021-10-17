9831f7f68801e306c8e082e4ac3c5d8e1e78b5e4
leenawon <nawon5154@gmail.com>
2021-10-17 18:18:30 +0900

    contacts에 id 추가 & localStorage에 저장하고 불러오기

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { v4 as uuid_v4 } from 'uuid';
 import './App.css';
 import AddContact from './components/AddContact';
 import ContactList from './components/ContactList';
 import Header from './components/Header';
 
 function App() {
+  const LOCAL_STORAGE_KEY = 'contacts';
   const [contacts, setContacts] = useState([]);
 
   function addContact(contact) {
-    setContacts([...contacts, contact]);
+    setContacts([...contacts, {id: uuid_v4(), ...contact}]);
   }
+
+  useEffect(() => {
+    const getContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    
+    if(getContacts) {
+      setContacts(getContacts);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
   return (
     <div className="ui container">
