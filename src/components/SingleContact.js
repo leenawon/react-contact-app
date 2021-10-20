4013ca241ddb65b220da2ae55ade0a63f8c01c0e
leenawon <nawon5154@gmail.com>
2021-10-21 03:38:00 +0900

    SingleContact component가 props 받아서 삭제할 id 전달

--- src/components/SingleContact.js
+++ src/components/SingleContact.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import profileImage from '../images/contact-image.jpg';
 
-function SingleContact({contact}) {
+function SingleContact(props) {
+  const contact = props.contact;
+  const id = contact.id;
+
   return (
-    <div className="item" key={contact.id}>
+    <div className="item" key={id}>
       <img className="ui avatar image" src={profileImage} alt="프로필 이미지"/>
       <div className="content">
         <h4 className="header">{contact.name}</h4>
         <span>{contact.email}</span>
       </div>
-      <i className="trash alternate outline icon" style={{color:"red"}}></i>
+      <i className="trash alternate outline icon" onClick={() => props.deleteContact(id)}></i>
     </div>
   )
 }
