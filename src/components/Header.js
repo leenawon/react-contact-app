8a7152d6cb45aefb75f62e17418fa5dc0c649e61
leenawon <nawon5154@gmail.com>
2021-10-24 00:05:27 +0900

    Header영역 Link 연결 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <div className="ui container">
+      <Link to="/">
         <h2 className="ui center aligned grey icon header">
           <i className="circular users icon"></i>
           나만의 연락처 리스트
         </h2>
+      </Link>
     </div>
   )
 }
