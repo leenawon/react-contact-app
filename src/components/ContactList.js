1e4964bcdb2f57abbec5efec08afe79bb53529ee
leenawon <nawon5154@gmail.com>
2021-10-23 17:14:45 +0900

    ContactList component에 add contact 버튼 추가하고 Link로 연결

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import SingleContact from './SingleContact';
 
 function ContactList(props) {
@@ -9,12 +10,20 @@ function ContactList(props) {
   }
 
   return (
-    <div className="ui celled list">
-      <h4>연락처</h4>
-      {contacts.map((contact) => (
-        <SingleContact key={contact.id} contact={contact} deleteContact={deleteContact} />
-      ))}
-    </div>
+    <>
+      <Link to="/add-contact">
+        <button className="ui basic button">
+          <i className="icon user"></i>
+          연락처 추가
+        </button>
+      </Link>
+      <div className="ui celled list">
+        <h4>연락처</h4>
+        {contacts.map((contact) => (
+          <SingleContact key={contact.id} contact={contact} deleteContact={deleteContact} />
+        ))}
+      </div>
+    </>
   )
 }
 
