e1bc16ee70783af9b1d77969e4089d3a80d99169
leenawon <nawon5154@gmail.com>
2021-10-27 20:18:26 +0900

    각각의 연락처에 Link 연결 추가(state로 값 전달)

--- src/components/SingleContact.js
+++ src/components/SingleContact.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import profileImage from '../images/contact-image.jpg';
 
 function SingleContact(props) {
-  const contact = props.contact;
-  const id = contact.id;
+  const {id, name, email} = props.contact;
 
   return (
     <div className="item" key={id}>
       <img className="ui avatar image" src={profileImage} alt="프로필 이미지"/>
       <div className="content">
-        <h4 className="header">{contact.name}</h4>
-        <span>{contact.email}</span>
+        <Link to={{pathname: `/contact/${id}`, state:{contact: props.contact}}}>
+          <h4 className="header">{name}</h4>
+          <span>{email}</span>
+        </Link>
       </div>
       <i className="trash alternate outline icon" onClick={() => props.deleteContact(id)}></i>
     </div>
