0c2b3e455d29e019d6b21196387c2c270657a4f4
leenawon <nawon5154@gmail.com>
2021-10-28 18:43:10 +0900

    confirm으로 한번 더 물어보고 삭제 진행하도록 변경

--- src/components/SingleContact.js
+++ src/components/SingleContact.js
@@ -5,6 +5,12 @@ import profileImage from '../images/contact-image.jpg';
 function SingleContact(props) {
   const {id, name, email} = props.contact;
 
+  function deleteButtonHandler() {
+    if(window.confirm("연락처를 삭제하시겠습니까?") === true) {
+      props.deleteContact(id)
+    }
+  }
+
   return (
     <div className="item" key={id}>
       <img className="ui avatar image" src={profileImage} alt="프로필 이미지"/>
@@ -14,7 +20,7 @@ function SingleContact(props) {
           <span>{email}</span>
         </Link>
       </div>
-      <i className="trash alternate outline icon" onClick={() => props.deleteContact(id)}></i>
+      <i className="trash alternate outline icon" onClick={deleteButtonHandler}></i>
     </div>
   )
 }
