376fe09e0297014ad4f8f823772f3aaae5aa8918
leenawon <nawon5154@gmail.com>
2021-12-03 19:24:06 +0900

    ContactList 검색 결과에 맞게 보여주도록 수정

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import SingleContact from './SingleContact';
 
 function ContactList(props) {
   const contacts = props.contacts;
+  const inputElement = useRef("");
+
+  function getSearchWord() {
+    props.searchWord(inputElement.current.value);
+  }
 
   function deleteContact(id) {
     props.removeContact(id);
@@ -19,9 +24,15 @@ function ContactList(props) {
       </Link>
       <div className="ui celled list">
         <h4>연락처</h4>
-        {contacts.map((contact) => (
+        <div className="ui search">
+          <div className="ui icon input">
+            <input ref={inputElement} type="text" className="prompt" value={props.search} placeholder="연락처를 검색하세요" onChange={getSearchWord}/>
+            <i className="search icon"></i>
+          </div>
+        </div>
+        {contacts.length > 0 ? contacts.map((contact) => (
           <SingleContact key={contact.id} contact={contact} deleteContact={deleteContact} />
-        ))}
+        )) : "검색 결과가 없습니다"}
       </div>
     </>
   )
